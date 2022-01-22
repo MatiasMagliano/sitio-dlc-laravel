712331234e2c210d0c0b68f57c506cc79a6bb14c
MatiasMagliano <86861518+MatiasMagliano@users.noreply.github.com>
2022-01-22 13:34:52 -0300

    Update 2021_12_22_013828_create_presentacions_table.php
    Se actualiza la migración, para que refleje diagrama de clases

--- database/migrations/2021_12_22_013828_create_presentacions_table.php
+++ database/migrations/2021_12_22_013828_create_presentacions_table.php
@@ -17,6 +17,8 @@ class CreatePresentacionsTable extends Migration
             $table->id();
             $table->string('forma');
             $table->string('presentacion');
+            $table->bool('hospitalario');
+            $table->bool('trazabilidad');
             $table->timestamps();
         });
     }
