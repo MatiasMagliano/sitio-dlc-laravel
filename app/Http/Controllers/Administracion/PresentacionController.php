f14d070fb76631e354650d8939f734a204e2adf7
MatiasMolina000 <matias_molina@mi.unc.edu.ar>
2022-01-31 19:18:01 -0300

    Se creo la vista index de seccion presentacion

--- app/Http/Controllers/Administracion/PresentacionController.php
+++ app/Http/Controllers/Administracion/PresentacionController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Administracion;
 
 use App\Http\Controllers\Controller;
+use App\Models\Presentacion;
 use Illuminate\Http\Request;
 
 class PresentacionController extends Controller
 {
-    //
+    public function index()
+    {
+        $presentaciones = Presentacion::all();
+        return view('administracion.presentaciones.index', compact('presentaciones'));
+    }
+
 }
