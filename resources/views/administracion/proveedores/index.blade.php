c03079e6658e4b9ec564ff72a0383215bdd3ad4f
MarceNunez90 <m.nunez@mi.unc.edu.ar>
2022-02-11 17:09:47 -0300

    Datos completos en el index

--- resources/views/administracion/proveedores/index.blade.php
+++ resources/views/administracion/proveedores/index.blade.php
@@ -17,7 +17,92 @@
 @stop
 
 @section('content')
-@stop
+
+    <x-adminlte-card>
+        <table id="tabla2" class="table table-bordered display nowrap" style="width: 100%;"> 
+            <thead>
+                <th>Razón social</th>
+                <th>Cuit</th>
+                <th>contacto</th>
+                <th>Dirección</th>
+            </thead>
+    
+            
+
+            @foreach($proveedores as $proveedor)
+                <tr>
+                    <td width="200px" style="vertical-align: middle;">{{$proveedor->razonSocial}}</td>
+                    <td width="200px" style="vertical-align: middle;">{{$proveedor->cuit}}</td>
+                    <td width="200px" style="vertical-align: middle;">{{$proveedor->contacto}}</td>
+                    <td width="200px" style="vertical-align: middle;">{{$proveedor->direccion}}</td>
+                </tr>
+            @endforeach
+        </table>
+    </x-adminlte-card>
+@endsection
+
+@section('js')
+    <script type="text/javascript" src="{{ asset('js/datatables-spanish.js') }}" defer></script>
+    <script>
+        $(document).ready(function() {
+            // el datatable es responsivo y oculta columnas de acuerdo al ancho de la pantalla
+            $('#tabla2').DataTable({
+                dom: 'Bfrtip',
+                buttons: [{
+                        extend: 'copyHtml5',
+                        text: 'Copiar al portapapeles',
+                    },
+                    {
+                        extend: 'excelHtml5',
+                        exportOptions: {
+                            columns: ':visible'
+                        }
+                    },
+                    {
+                        extend: 'print',
+                        text: 'Imprimir',
+                        exportOptions: {
+                            columns: [0, 1, 2, 3]
+                        }
+                    },
+                    {
+                        extend: 'pdfHtml5',
+                        exportOptions: {
+                            columns: [0, 1, 2, 3]
+                        }
+                    },
+                    {
+                        extend: 'colvis',
+                        text: 'Seleccionar columnas'
+                    }
+                ],
+                responsive: {
+                    details: {
+                        renderer: function(api, rowIdx, columns) {
+                            var data = $.map(columns, function(col, i) {
+                                return col.hidden ?
+                                    '<tr data-dt-row="' + col.rowIndex + '" data-dt-column="' +
+                                    col.columnIndex + '">' +
+                                    '<td>' + col.title + ':' + '</td> ' +
+                                    '<td>' + col.data + '</td>' +
+                                    '</tr>' :
+                                    '';
+                            }).join('');
+
+                            return data ?
+                                $('<table/>').append(data) :
+                                false;
+                        }
+                    }
+                }
+            });
+        });
+    </script>
+@endsection    
+    
+
+
+
 
 @section('footer')
     <strong>AUSI - ESCMB - UNC - <a href="https://mb.unc.edu.ar/" target="_blank">mb.unc.edu.ar</a></strong>
