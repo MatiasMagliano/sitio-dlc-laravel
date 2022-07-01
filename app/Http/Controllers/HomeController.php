18db3251703f4359e76e6591d424617bddd96399
MatiasMolina000 <matias_molina@mi.unc.edu.ar>
2022-07-01 00:39:41 -0300

    Gráfico de barras apiladas de perdidas por vencimiento. Perdidas real y perdidas en caso de venta al costo. Se toma en cuenta el precio de compra y el menor precio actual de cada producto Se agrega en HomeController y Home.Blade: la consulta, el contenedor y el diseño del gráfico

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -84,7 +84,52 @@ class HomeController extends Controller
         $cotizAprobRechaz = json_encode($cotizAprobRechaz);
         //dd($cotizAprobRechaz);
 
-        return view('home', compact('cantOT', 'maxCotizaciones', 'vencimientos', 'cantCotiz', 'compras', 'cotizAprobRechaz')
+        //LÓGICA PARA EL GRÁFICO DE BARRAS perdidas-vencimiento
+        
+        $perdidasPorVencimiento = DB::select(DB::raw("
+            SELECT VTO.Vencimiento, SUM(VTO.Perdida) AS `Perdida`, SUM(CASE WHEN VTO.costo1 < VTO.Perdida THEN 0 ELSE (VTO.costo1 - VTO.Perdida) END) AS `Oportunidad`
+            FROM `lotes` lo
+                INNER JOIN(SELECT lo.id, lpp.producto_id, lpp.presentacion_id, (`cantidad` * `precio_compra`) AS `Perdida`, 
+                        LEFT(`fecha_vencimiento`, 7) AS Vencimiento,  (`cantidad` * lipro.costo) AS `costo1`
+                    FROM `lotes` lo
+                        INNER JOIN `lote_presentacion_producto` lpp ON lo.id = lpp.lote_id
+                        INNER JOIN `lista_precios` lipro ON lpp.producto_id = lipro.producto_id 
+                        INNER JOIN `lista_precios` lipre ON lpp.presentacion_id = lipre.presentacion_id 
+                    WHERE lipro.costo = lipre.costo
+                )VTO ON lo.id = VTO.id
+            WHERE lo.fecha_vencimiento BETWEEN DATE_SUB(NOW(),INTERVAL 6 MONTH) AND DATE_ADD(NOW(),INTERVAL 7 DAY)
+            GROUP BY VTO.Vencimiento ORDER BY VTO.Vencimiento ASC;
+        "));
+        $perdidasPorVencimiento = array(
+            'label' => [
+                $perdidasPorVencimiento[0]->Vencimiento,
+                $perdidasPorVencimiento[1]->Vencimiento,
+                $perdidasPorVencimiento[2]->Vencimiento,
+                $perdidasPorVencimiento[3]->Vencimiento,
+                $perdidasPorVencimiento[4]->Vencimiento,
+                $perdidasPorVencimiento[5]->Vencimiento,
+            ],
+            'dataOportunidad'  => [
+                $perdidasPorVencimiento[0]->Oportunidad,
+                $perdidasPorVencimiento[1]->Oportunidad,
+                $perdidasPorVencimiento[2]->Oportunidad,
+                $perdidasPorVencimiento[3]->Oportunidad,
+                $perdidasPorVencimiento[4]->Oportunidad,
+                $perdidasPorVencimiento[5]->Oportunidad,
+            ],
+            'dataPerdida'  => [
+                $perdidasPorVencimiento[0]->Perdida,
+                $perdidasPorVencimiento[1]->Perdida,
+                $perdidasPorVencimiento[2]->Perdida,
+                $perdidasPorVencimiento[3]->Perdida,
+                $perdidasPorVencimiento[4]->Perdida,
+                $perdidasPorVencimiento[5]->Perdida,
+            ],
+
+        );
+        $perdidasPorVencimiento = json_encode($perdidasPorVencimiento);
+        
+        return view('home', compact('cantOT', 'maxCotizaciones', 'vencimientos', 'cantCotiz', 'compras', 'cotizAprobRechaz', 'perdidasPorVencimiento')
         );
     }
 }
