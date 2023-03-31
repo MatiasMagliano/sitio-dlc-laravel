4dc172853b45d17f1946c071d3ce92b023c1968b
MatiasMagliano <matias.magliano@mi.unc.edu.ar>
2023-03-31 20:17:48 -0300

    Crea querys para listados

--- database/seeders/ListadosSeeder.php
+++ database/seeders/ListadosSeeder.php
@@ -19,15 +19,16 @@ class ListadosSeeder extends Seeder
         [
             [
                 'nombre'  => 'Productos sin stock por proveedor',
-                'query'   => 'SELECT * FROM listados'
+                'query'   => 'SELECT productos.droga, CONCAT(presentacions.forma, " ", presentacions.presentacion) AS "presentacion", proveedors.razon_social, lotes.precio_compra FROM lote_presentacion_producto
+                INNER JOIN productos on productos.id = lote_presentacion_producto.producto_id INNER JOIN presentacions on presentacions.id = lote_presentacion_producto.presentacion_id INNER JOIN lotes ON lotes.id = lote_presentacion_producto.id INNER JOIN lista_precios on lista_precios.producto_id = lote_presentacion_producto.producto_id AND lista_precios.presentacion_id = lote_presentacion_producto.presentacion_id INNER JOIN proveedors ON proveedors.id = lista_precios.proveedor_id WHERE lotes.cantidad = 0 GROUP BY proveedors.razon_social;'
             ],
             [
                 'nombre'  => 'Producto por vencimientos a largo plazo',
-                'query'   => 'SELECT * FROM listados'
+                'query'   => 'SELECT productos.droga, CONCAT(presentacions.forma, " ", presentacions.presentacion) AS presentación, TIMESTAMPDIFF(MONTH, lotes.fecha_vencimiento, lotes.fecha_compra)AS días FROM lote_presentacion_producto INNER JOIN productos on productos.id = lote_presentacion_producto.producto_id INNER JOIN presentacions on presentacions.id = lote_presentacion_producto.presentacion_id INNER JOIN lotes on lotes.id = lote_presentacion_producto.lote_id ORDER BY días DESC;'
             ],
             [
-                'nombre'  => 'Proveedores por volumen mayor de productos',
-                'query'   => 'SELECT * FROM listados'
+                'nombre'  => 'Proveedores por mayor volumen de productos',
+                'query'   => 'SELECT proveedors.razon_social, COUNT(productos.id) AS cant_productos FROM lote_presentacion_producto INNER JOIN productos on productos.id = lote_presentacion_producto.producto_id INNER JOIN presentacions on presentacions.id = lote_presentacion_producto.presentacion_id INNER JOIN lotes ON lotes.id = lote_presentacion_producto.id INNER JOIN lista_precios on lista_precios.producto_id = lote_presentacion_producto.producto_id AND lista_precios.presentacion_id = lote_presentacion_producto.presentacion_id INNER JOIN proveedors ON proveedors.id = lista_precios.proveedor_id GROUP BY proveedors.razon_social ORDER BY `cant_productos` DESC '
             ],
             [
                 'nombre'  => 'Productos y sus vencimientos',
