08711f1044308beca0ddb09666d8f18f20fa521b
MatiasMagliano <matias.magliano@mi.unc.edu.ar>
2023-04-03 21:48:37 -0300

    Modificación semillado de ListaPrecios

--- database/seeders/ListaPrecioSeeder.php
+++ database/seeders/ListaPrecioSeeder.php
@@ -3,9 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\ListaPrecio;
+use App\Models\Lote;
 use Illuminate\Database\Seeder;
 use App\Models\LotePresentacionProducto;
 use App\Models\Proveedor;
+use Faker\Factory as Faker;
 
 class ListaPrecioSeeder extends Seeder
 {
@@ -16,6 +18,29 @@ class ListaPrecioSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //se genera el faker
+        $this->faker = Faker::create();
+        $productos = LotePresentacionProducto::select('producto_id', 'presentacion_id')->groupBy(['producto_id', 'presentacion_id'])->get();
+
+        foreach($productos as $lpp)
+        {
+            $max_proveedor = rand(1, 5);
+            for($i = 0; $i < $max_proveedor; $i++)
+            {
+                $codigoProv =  $this->faker->numberBetween(1000000, 9999999);
+                $proveedor = Proveedor::inRandomOrder()->first()->id;
+                // se calcula entre un 10 y un 30% más de costo con respecto al precio_compra del lote
+                $promedio = Lote::promedioPrecioLotes($lpp->producto_id, $lpp->presentacion_id);
+                $precio = $promedio + $promedio * rand(10, 30) / 100;
+
+                ListaPrecio::create([
+                    'codigoProv' => $codigoProv,
+                    'producto_id' => $lpp->producto_id,
+                    'presentacion_id' => $lpp->presentacion_id,
+                    'proveedor_id' => $proveedor,
+                    'costo' => $precio
+                ]);
+            }
+        }
     }
 }
