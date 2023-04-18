e0181c073ff51f00b08541948c3011574434b416
MatiasMagliano <matias.magliano@mi.unc.edu.ar>
2023-04-18 13:31:18 -0300

    Actualiza paquetes

--- config/app.php
+++ config/app.php
@@ -174,7 +174,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Kyslik\ColumnSortable\ColumnSortableServiceProvider::class,
+        #Kyslik\ColumnSortable\ColumnSortableServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
         /*
          * Application Service Providers...
@@ -184,7 +184,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
+        #Maatwebsite\Excel\ExcelServiceProvider::class,
     ],
 
     /*
@@ -239,8 +239,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Excel' => Maatwebsite\Excel\Excel::class,
-        'Excel' => \Maatwebsite\Excel\Facades\Excel::class,
+        #'Excel' => Maatwebsite\Excel\Excel::class,
+        #'Excel' => \Maatwebsite\Excel\Facades\Excel::class,
         'PDF' => \Barryvdh\DomPDF\Facade\Pdf::class,
     ],
 
