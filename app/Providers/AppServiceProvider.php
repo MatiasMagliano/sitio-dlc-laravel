d8c34bb4280184305c74ddebce314bf9724a4690
MatiasMagliano <matias.magliano@mi.unc.edu.ar>
2023-04-28 14:46:25 -0300

    se agrega locale es_ES a la base de datos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Carbon\Carbon;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,6 +32,7 @@ class AppServiceProvider extends ServiceProvider
         // Configuración para fechas en español
         Carbon::setUTF8(true);
         Carbon::setLocale(config('app.locale'));
+        DB::statement("SET lc_time_names = 'es_ES'");
         setlocale(LC_ALL, 'es_ES', 'es', 'ES', 'es_ES.utf8');
     }
 }
