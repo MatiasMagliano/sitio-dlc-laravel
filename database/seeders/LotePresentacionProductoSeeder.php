3e70b118115be1c5cde47782b8106d98b7c4c0f2
MatiasMagliano <matias.magliano@mi.unc.edu.ar>
2023-09-03 23:40:11 -0300

    Agrega LOTE DUMMY inicial

--- database/seeders/LotePresentacionProductoSeeder.php
+++ database/seeders/LotePresentacionProductoSeeder.php
@@ -8,12 +8,25 @@ use App\Models\Lote;
 use App\Models\LotePresentacionProducto;
 use App\Models\Presentacion;
 use App\Models\Producto;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class LotePresentacionProductoSeeder extends Seeder
 {
     public function run()
     {
+        // se crea un lote dummy para cuando se crean productos sin lote
+        $fecha_fake = Carbon::create(1970, 1, 1, 12);
+        Lote::create([
+            'id'                => 1,
+            'identificador'     => 'LOTE DUMMY',
+            'precio_compra'     => 0,
+            'fecha_elaboracion' => $fecha_fake,
+            'fecha_compra'      => $fecha_fake,
+            'fecha_vencimiento' => $fecha_fake,
+            'cantidad'          => 0,
+            'deleted_at'        => $fecha_fake
+        ]);
 
         // se hace de esta manera, para evitar lotes duplicados en productos diferentes
         foreach (Producto::all() as $producto){
