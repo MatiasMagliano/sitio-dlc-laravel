ae269600426e1a88d81716f1022629fba851f760
MatiasMagliano <matias.magliano@mi.unc.edu.ar>
2023-09-06 09:34:06 -0300

    Corrige problemas de formato

--- resources/views/administracion/productos/partials/form-edit.blade.php
+++ resources/views/administracion/productos/partials/form-edit.blade.php
@@ -21,10 +21,9 @@
                 <label for="input-antigua_droga">
                     <input id="input-antigua_droga" name="antigua_droga" value="{{ $producto->droga }}" hidden>
                 </label>
-                <label>Droga *
-                    <input type="text" name="droga" id="droga" class="form-control" placeholder="Nombre genérico o compuesto principal representativo" value="{{ old('droga', $producto->droga) }}" autofocus>
-                    <div class="invalid-feedback" id="invalid-feedback-droga"></div>
-                </label>
+                <label for="droga">Droga *</label>
+                <input type="text" name="droga" id="droga" class="form-control" placeholder="Nombre genérico o compuesto principal representativo" value="{{ old('droga', $producto->droga) }}" autofocus>
+                <div class="invalid-feedback" id="invalid-feedback-droga"></div>
             </div>
         </div>
     </div>
@@ -36,12 +35,13 @@
         <div class="card-header">
             <div class="row d-flex">
                 <div class="col-8">
-                    <h6 class="heading-small text-muted mb-1" id="header-edit-prod-pres">EDITAR PRESENTACION</h6>
+                    <h6 class="heading-small text-muted mb-1" id="header-edit-prod-pres">PRESENTACION DEL PRODUCTO</h6>
                 </div>
                 <div class="col-4 text-right">
-                    <label>Nueva presentación 
-                        <input type="checkbox" id="crear_editar" name="crear_editar" {{old('crear_editar') == 1 ? 'checked' : ''}}>
-                    </label>
+                    <div class="form-check">
+                        <input type="checkbox" id="crear_editar" name="crear_editar" {{old('crear_editar') == 1 ? 'checked' : ''}} class="form-check-input">
+                        <label for="crear_editar">Nueva presentación</label>
+                    </div>
                 </div>
             </div>
         </div>
@@ -49,72 +49,67 @@
             <div class="pl-lg-4">
                 <div class="form-group" id="cambio-presentacion">
                     {{-- Antigua presentacion --}}
-                    <label for="input-antigua_presentacion">
-                        <input id="input-antigua_presentacion" name="antigua_presentacion" value="{{$presentacion->id}}" hidden>
-                    </label>
-                    
-                    <label>Forma farmacéutica y presentación *
-                        <select name="presentacion" id="input-presentacion"class="selecion-presentacion form-control-alternative @error('presentacion') is-invalid @enderror">
-                            <option data-placeholder="true"></option>
-                            @foreach ($presentaciones as $presentacion_all)
-                                @if ($presentacion_all->id == old('presentacion', $presentacion->id))
-                                    <option value="{{ $presentacion_all->id }}" selected>
-                                        @if ($presentacion_all->hospitalario)
-                                            <b>H - </b>
-                                        @endif
-                                        {{ $presentacion_all->forma }}, {{ $presentacion_all->presentacion }}
-                                        @if ($presentacion_all->trazabilidad)
-                                            <b> - Trazable</b>
-                                        @endif
-                                    </option>
-                                @else
-                                    <option value="{{ $presentacion_all->id }}">
-                                        @if ($presentacion_all->hospitalario)
-                                            <b>H - </b>
-                                        @endif
-                                        {{ $presentacion_all->forma }}, {{ $presentacion_all->presentacion }}
-                                        @if ($presentacion_all->trazabilidad)
-                                            <b> - Trazable</b>
-                                        @endif
-                                    </option>
-                                @endif
-                            @endforeach
-                        </select>
-                        @error('presentacion')<div class="invalid-feedback">{{$message}}</div>@enderror
-                    </label>
+                    <input id="input-antigua_presentacion" name="antigua_presentacion" value="{{$presentacion->id}}" hidden>
+
+                    <label>Forma farmacéutica y presentación *</label>
+                    <select name="presentacion" id="input-presentacion"class="selecion-presentacion form-control-alternative @error('presentacion') is-invalid @enderror">
+                        <option data-placeholder="true"></option>
+                        @foreach ($presentaciones as $presentacion_all)
+                            @if ($presentacion_all->id == old('presentacion', $presentacion->id))
+                                <option value="{{ $presentacion_all->id }}" selected>
+                                    @if ($presentacion_all->hospitalario)
+                                        <b>H - </b>
+                                    @endif
+                                    {{ $presentacion_all->forma }}, {{ $presentacion_all->presentacion }}
+                                    @if ($presentacion_all->trazabilidad)
+                                        <b> - Trazable</b>
+                                    @endif
+                                </option>
+                            @else
+                                <option value="{{ $presentacion_all->id }}">
+                                    @if ($presentacion_all->hospitalario)
+                                        <b>H - </b>
+                                    @endif
+                                    {{ $presentacion_all->forma }}, {{ $presentacion_all->presentacion }}
+                                    @if ($presentacion_all->trazabilidad)
+                                        <b> - Trazable</b>
+                                    @endif
+                                </option>
+                            @endif
+                        @endforeach
+                    </select>
+                    @error('presentacion')<div class="invalid-feedback">{{$message}}</div>@enderror
                 </div>
                 <div class="form-group" id="nueva-presentacion" hidden>
-                    
                     <div class="row d-flex m-3">
                         <div class="form-group col">
-                            <label>Forma *
-                                <input type="text" name="nuevaforma" id="nuevaforma" class="form-control" placeholder="Nombre completo de la forma" value="{{ old('forma') }}" autofocus>
-                                <div class="invalid-feedback" id="invalid-feedback-nuevaforma"></div>
-                            </label>
+                            <label>Forma *</label>
+                            <input type="text" name="nuevaforma" id="nuevaforma" class="form-control" placeholder="Nombre completo de la forma" value="{{ old('forma') }}" autofocus>
+                            <div class="invalid-feedback" id="invalid-feedback-nuevaforma"></div>
                         </div>
                     </div>
-                    
+
                     <div class="row d-flex m-3">
                         <div class="form-group col">
-                            <label>Presentacion *
-                                <input type="text" name="nuevapresentacion" id="nuevapresentacion" class="form-control" placeholder="Nombre completo de la presentacion" value="{{ old('nuevapresentacion') }}" autofocus>
-                                <div class="invalid-feedback" id="invalid-feedback-nuevapresentacion"></div>
-                            </label>
+                            <label>Presentacion *</label>
+                            <input type="text" name="nuevapresentacion" id="nuevapresentacion" class="form-control" placeholder="Nombre completo de la presentacion" value="{{ old('nuevapresentacion') }}" autofocus>
+                            <div class="invalid-feedback" id="invalid-feedback-nuevapresentacion"></div>
                         </div>
                     </div>
                     <div class="row d-flex m-3">
-                        <div class="form-group col">
-                            <label>
-                                <input type="checkbox" id="checkboxTrazable" name="checkboxTrazable" value="1">Trazable
-                            </label>
-                            <br>
-                            <label>
-                                <input type="checkbox" id="checkboxDivisible" name="checkboxDivisible" value="1">Divisible
-                            </label>
-                            <br>
-                            <label>
-                                <input type="checkbox" id="checkboxHospitalario" name="checkboxHospitalario" value="1">Hospitalario
-                            </label>
+                        <div class="col">
+                            <div class="form-check">
+                                <input type="checkbox" id="checkboxTrazable" name="checkboxTrazable" value="1" class="form-check-input">
+                                <label for="checkboxTrazable" class="form-check-label">Trazable</label>
+                            </div>
+                            <div class="form-check">
+                                <input type="checkbox" id="checkboxDivisible" name="checkboxDivisible" value="1" class="form-check-input">
+                                <label for="checkboxDivisible" class="form-check-label">Divisible</label>
+                            </div>
+                            <div class="form-check">
+                                <input type="checkbox" id="checkboxHospitalario" name="checkboxHospitalario" value="1" class="form-check-input">
+                                <label for="checkboxHospitalario" class="form-check-label">Hospitalario</label>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -127,7 +122,7 @@
         <div class="card-header">
             <div class="row d-flex">
                 <div class="col-8">
-                    <h6 class="heading-small text-muted mb-1">PROVEEDOR/ES ({{$proveedor->count()}})</h6>
+                    <h6 class="heading-small text-muted mb-1">PROVEEDOR/ES DEL PRODUCTO</h6>
                 </div>
                 <div class="col-4 text-right">
                     {{-- <button type="button" class="btn btn-sm btn-info" data-toggle="modal"
